494387af47c863381b744cbf274f699b6d375056
Attasit Datsong <attasitd@hotmail.com>
2026-03-06 11:34:30 +0700

    Update __init__.py

--- components/lora_radiolib/__init__.py
+++ components/lora_radiolib/__init__.py
@@ -1,14 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_FREQUENCY # เพิ่ม CONF_FREQUENCY
 
-# บังคับโหลด SPI และ Text Sensor
 DEPENDENCIES = ['spi']
 AUTO_LOAD = ['text_sensor']
 
 lora_ns = cg.esphome_ns.namespace('lora_radiolib')
-# เรายังใช้ชื่อคลาสเดิมได้ครับ แต่ข้างในจะเปลี่ยนไปใช้ LoRa.h
 LoRaRadioLib = lora_ns.class_('LoRaRadioLib', text_sensor.TextSensor, cg.Component)
 
 CONF_CS_PIN = 'cs_pin'
@@ -19,10 +17,10 @@ CONFIG_SCHEMA = text_sensor.text_sensor_schema(LoRaRadioLib).extend({
     cv.Required(CONF_CS_PIN): cv.int_,
     cv.Required(CONF_DIO0_PIN): cv.int_,
     cv.Required(CONF_RST_PIN): cv.int_,
+    cv.Optional(CONF_FREQUENCY, default=433.0): cv.float_, # เพิ่มบรรทัดนี้กลับมา
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    # เปลี่ยนมาโหลด Library LoRa ของ Sandeep Mistry
     cg.add_library("sandeepmistry/LoRa", "0.8.0")
     var = cg.new_Pvariable(config[CONF_ID])
     
@@ -30,3 +28,6 @@ async def to_code(config):
     await text_sensor.register_text_sensor(var, config)
     
     cg.add(var.set_pins(config[CONF_CS_PIN], config[CONF_DIO0_PIN], config[CONF_RST_PIN]))
+    # เพิ่มบรรทัดส่งค่าความถี่ไปที่ C++
+    if CONF_FREQUENCY in config:
+        cg.add(var.set_frequency(config[CONF_FREQUENCY]))
